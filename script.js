ec48548280d2e8f3d1b31cdf61a6871a6c3af1d8
pwnont <87240253+pwnont@users.noreply.github.com>
2024-05-09 10:58:12 +0700

    start

---
+++ script.js
@@ -0,0 +1,40 @@
+let count = 0; 
+
+function checkAnswer1(answer) {
+    const result = document.getElementById('result1');
+    if (answer === 'a') {
+        result.textContent = "ถูกต้อง PIM ย่อมาจาก PIM";
+        result.style.color = 'green';
+        count++;
+    } else {
+        result.textContent = "Incorrect! Try again.";
+        result.style.color = 'red';
+    }
+}
+
+function checkAnswer2(answer) {
+    const result = document.getElementById('result2');
+    const result3 = document.getElementById('result3');
+    if (answer === 'd') {
+        result.textContent = "ถูกต้อง PIM ก่อตั้งขึ้นเมื่อปี 2550";
+        result.style.color = 'green';
+        count++;
+    } else {
+        result.textContent = "Incorrect! Try again.";
+        result.style.color = 'red';
+    }
+    result3.textContent = "คุณได้ทั้งหมด " + count + " คะแนน";
+    count = 0;
+}
+
+function checkAnswer3(answer) {
+    const result = document.getElementById('result4');
+    if (answer === 'b') {
+        result.textContent = "ถูกต้อง";
+        result.style.color = 'green';
+        count++;
+    } else {
+        result.textContent = "Incorrect! Try again.";
+        result.style.color = 'red';
+    }
+}
